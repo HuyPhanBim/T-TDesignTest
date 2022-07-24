d4dd60a0f12dccf16ffb509c3d7f13a687cd5803
HUY <81575874+HuyPhanBim@users.noreply.github.com>
2022-07-24 19:26:02 +0700

    upload

---
+++ PVHuy/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PVHuy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            // Input Number
+            int N = Program.InputNumber();
+            // Input Matrix
+            int[,] Matrix = Program.InputMatrix(N);
+
+            // Find a submatrix with two equal diagonals
+            int CheckContinue = 0;
+            for(int a=0;a<N-1;a++)
+            {
+                for (int m = 0; m < a + 1; m++)
+                {
+                    for (int n = 0; n < a + 1; n++)
+                    {
+
+                        int[,] Submatrix = new int[N - a, N - a];
+                        for (int i = 0; i < N - a; i++)
+                        {
+                            for (int j = 0; j < N - a; j++)
+                            {
+
+                                Submatrix[i, j] = Matrix[i + m, j + n];
+                            }
+                        }
+                        if(CheckTotalDiagonal(Submatrix))
+                        {
+                            CheckContinue = CheckContinue + 1;
+                            Console.WriteLine("Kết quả là: ");
+                            Program.PrintMatrix(Submatrix);
+                        }
+                        if (CheckContinue == 1)
+                        {
+                            break;
+                        }    
+                    }
+                    if (CheckContinue == 1)
+                    {
+                        break;
+                    }
+                }
+                if (CheckContinue == 1)
+                {
+                    break;
+                }
+            }
+            if(CheckContinue == 0)
+            {
+                Console.WriteLine("Không có ma trận con nào thỏa mãn bài toán");
+            }
+        }
+
+
+        static int InputNumber()
+        {
+            int N;
+            InputN:
+            Console.WriteLine("Nhập vào số tự nhiên n: ");
+            string firstInput = Console.ReadLine();
+            try
+            {
+                N = Int32.Parse(firstInput);
+                if (N < 2 || N > 20)
+                {
+                    Console.WriteLine("n trong khoảng từ 2 tới 20");
+                    goto InputN;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Đầu vào không phù hợp. Vui lòng nhập lại !");
+                goto InputN;
+            }
+            return N;
+        }
+
+        static int[,] InputMatrix(int N)
+        {
+            InputMatrix:
+            int[,] Matrix = new int[N, N];
+            Console.WriteLine("Nhập vào ma trận: " + N.ToString() + "*" + N.ToString() + " :");
+            for (int i = 0; i < N; i++)
+            {
+                string input = Console.ReadLine();
+                string[] inputStringArray = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < N; j++)
+                {
+                    try
+                    {
+                       if( inputStringArray.Length == N)
+                       {
+                            Matrix[i, j] = Int32.Parse(inputStringArray[j]);
+                       }
+                       else
+                       {
+                            Console.WriteLine("Đầu vào không phù hợp. Vui lòng nhập lại !");
+                            goto InputMatrix;
+                        }
+                }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Đầu vào không phù hợp. Vui lòng nhập lại !");
+                        goto InputMatrix;
+                    }
+                }
+            }
+
+            return Matrix;
+        }
+
+        static void PrintMatrix(int[,] Matrix)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write(Matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool CheckTotalDiagonal( int[,] Matrix)
+        {
+            int Td1= 0;
+            int Td2 = 0;
+
+            // Total Diagonal 1
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                Td1= Td1+ Matrix[i, i];
+            }
+
+            // Total Diagonal 2
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                Td2 = Td2 + Matrix[i, Matrix.GetLength(1)-i-1];
+            }
+
+            if (Td1== Td2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Question2/Question2_2020/SplitFloor.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Structure;
+
+
+namespace Question2_2020
+{
+    [Journaling(JournalingMode.NoCommandData), Regeneration(RegenerationOption.Manual), Transaction(TransactionMode.Manual)]
+    class SplitFloor : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            Floor MasterFloor;
+
+            try
+            {
+                MasterFloor = doc.GetElement(uidoc.Selection.PickObject(ObjectType.Element, new SelectionFloor(),"Selection Master Floor").ElementId) as Floor;
+            }
+            catch
+            {
+                return Result.Cancelled;
+            }
+
+            FloorType MasterFloorType = MasterFloor.FloorType;
+            Level MasterFloorLevel = doc.GetElement(MasterFloor.LevelId) as Level;
+            bool MasterFloorStructural;
+            if(MasterFloor.LookupParameter("Structural").AsDouble() == 0)
+            {
+                MasterFloorStructural = false;
+            }
+            else
+            {
+                MasterFloorStructural = true;
+            }
+
+            // Get List DB.Line from Floor
+            ElementClassFilter filter = new ElementClassFilter(typeof(Sketch));
+            ElementId sketchId = MasterFloor.GetDependentElements(filter).First();
+            Sketch floorSketch = doc.GetElement(sketchId) as Sketch;
+            CurveArray floorProfile = floorSketch.Profile.get_Item(0);
+            List<Line> listcurve = new List<Line>();
+            for(int i=0;i<floorProfile.Size;i++)
+            {
+                listcurve.Add(floorProfile.get_Item(i) as Line);
+            }
+
+            // Filter the lines parallel to the X axis,Y axis
+            List<Line> ListLineX = new List<Line>(); // list line parallel to the X axis
+            List<Line> ListLineY = new List<Line>(); // list line parallel to the Y axis
+            foreach (Line l in listcurve)
+            {
+                if(l.Direction.DotProduct(new XYZ(1,0,0)) == -1 || l.Direction.DotProduct(new XYZ(1, 0, 0)) == 1)
+                {
+                    ListLineX.Add(l);
+                }
+                else
+                {
+                    ListLineY.Add(l);
+                }
+            }
+
+            // Get points on the cut line
+            IEnumerable<Line> LineXGroupByY = ListLineX.GroupBy(l =>Math.Round(l.GetEndPoint(0).Y)).Select(grb => grb.ToList().First());
+            List<XYZ> ListpointX = new List<XYZ>();
+            foreach(Line l in LineXGroupByY)
+            {
+                ListpointX.Add(l.GetEndPoint(0));
+            }
+            List<XYZ> ListpointXsortY = ListpointX.OrderBy(P => P.Y).ToList();
+
+
+            // move list point X sort Y 
+            List<XYZ> Listpointcutcheck = new List<XYZ>();
+            foreach (XYZ p in ListpointXsortY)
+            {
+                Listpointcutcheck.Add(new XYZ(p.X, p.Y + 0.000001, p.Z));
+            }
+
+            // list boubdary sub floor
+            List<CurveArray> ListBoundary = new List<CurveArray>();
+
+            for (int n = 0; n< ListpointXsortY.Count() - 1; n++)
+            {
+                //find the lines that the clipping points see
+                XYZ PointCutCheck = Listpointcutcheck[n];
+                List<Line> LineYcut = new List<Line>();
+                foreach (Line l in ListLineY)
+                {
+                    XYZ PminY = new XYZ();
+                    XYZ PmaxY = new XYZ();
+                    if (l.GetEndPoint(0).Y > l.GetEndPoint(1).Y)
+                    {
+                        PminY = l.GetEndPoint(1);
+                        PmaxY = l.GetEndPoint(0);
+                    }
+                    else
+                    {
+                        PminY = l.GetEndPoint(0);
+                        PmaxY = l.GetEndPoint(1);
+                    }
+                    if (PointCutCheck.Y > PminY.Y & PointCutCheck.Y < PmaxY.Y)
+                    {
+                        LineYcut.Add(l);
+                    }
+                }
+
+                // get rectangular bottom line
+                XYZ PointCut = ListpointXsortY[n];
+                List<XYZ> RecBottomP = new List<XYZ>();
+                foreach (Line lineY in LineYcut)
+                {
+                    RecBottomP.Add(lineY.Project(PointCut).XYZPoint);
+                }
+
+                List<XYZ> RecBottomPSort = RecBottomP.OrderBy(p => p.X).ToList();
+
+                List<Line> RecButtomLines = new List<Line>();
+                for (int i = 0; i < (RecBottomPSort.Count() / 2); i++)
+                {
+                    RecButtomLines.Add(Line.CreateBound(RecBottomPSort[i * 2], RecBottomPSort[i * 2 + 1]));
+                }
+
+                // create boundrary sub floor
+                XYZ PointTop = ListpointXsortY[n+1];
+                foreach (Line lineButtom in RecButtomLines)
+                {
+                    XYZ PButoomStart = lineButtom.GetEndPoint(0);
+                    XYZ PButoomEnd = lineButtom.GetEndPoint(1);
+                    XYZ PTopStrart = new XYZ(PButoomEnd.X, PointTop.Y, PointTop.Z);
+                    XYZ PTopEnd = new XYZ(PButoomStart.X, PointTop.Y, PointTop.Z);
+
+                    Line lineside1 = Line.CreateBound(PButoomEnd, PTopStrart);
+                    Line linetop = Line.CreateBound(PTopStrart, PTopEnd);
+                    Line lineside2 = Line.CreateBound(PTopEnd, PButoomStart);
+
+                    CurveArray Boundary = new CurveArray();
+                    Boundary.Append(lineButtom);
+                    Boundary.Append(lineside1);
+                    Boundary.Append(linetop);
+                    Boundary.Append(lineside2);
+
+                    ListBoundary.Add(Boundary);
+                }
+            }
+
+
+            Transaction trans = new Transaction(doc);
+            trans.Start("Split Floor");
+
+            // create sub floors and delete master floor
+            foreach (CurveArray bondary in ListBoundary)
+            {
+                doc.Create.NewFloor(bondary, MasterFloorType, MasterFloorLevel, MasterFloorStructural);
+            }
+
+            doc.Delete(MasterFloor.Id);
+
+            trans.Commit();
+            return Result.Succeeded;
+        }
+    }
+
+    class SelectionFloor : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Floors)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
